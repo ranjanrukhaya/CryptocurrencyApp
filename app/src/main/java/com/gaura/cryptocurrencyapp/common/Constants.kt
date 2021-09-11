fe55121691ec09f07ab5229d7c69221cb28b6932
Ranjan Rukhaya <ranjan.rukhaya@gmail.com>
2021-09-11 18:35:20 +0530

    Crypto Currency App view models added

--- app/src/main/java/com/gaura/cryptocurrencyapp/common/Constants.kt
+++ app/src/main/java/com/gaura/cryptocurrencyapp/common/Constants.kt
@@ -1,4 +1,7 @@
 package com.gaura.cryptocurrencyapp.common
 
 object Constants {
+
+    const val PARAM_COIN_ID: String = "coinId"
+    const val BASE_URL = "https://api.coinpaprika.com/"
 }
---
+++ app/src/main/java/com/gaura/cryptocurrencyapp/presentation/coin_detail/CoinDetailState.kt
@@ -0,0 +1,9 @@
+package com.gaura.cryptocurrencyapp.presentation.coin_detail
+
+import com.gaura.cryptocurrencyapp.domain.model.CoinDetail
+
+data class CoinDetailState(
+    val isLoading: Boolean = false,
+    val coin: CoinDetail? = null,
+    val error: String = ""
+)
